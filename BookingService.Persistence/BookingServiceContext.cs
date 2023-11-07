ce4e51398f3be5ee5528d6d077f514d8e148e4f4
bartlomiejmikolajczuk@gmail.com <bartlomiejmikolajczuk@gmail.com>
2023-11-07 18:29:36 +0100

    dodanie projektów security, zmiana nazwy projektu warstwy dostępu do danych

--- BookingService.Persistence/BookingServiceContext.cs
+++ BookingService.Persistence/BookingServiceContext.cs
@@ -1,4 +1,5 @@
 ﻿using BookingService.Domain.Entities;
+using BookingService.Domain.Entities.Base;
 using Microsoft.EntityFrameworkCore;
 
 #nullable disable
@@ -21,6 +22,22 @@ namespace BookingService.Persistence
             modelBuilder.
                 ApplyConfigurationsFromAssembly
                 (typeof(BookingServiceContext).Assembly);
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseAuditableEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
