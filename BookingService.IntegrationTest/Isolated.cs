b3e94430b09979831c3098d8faeb1f1157233b5a
bartlomiejmikolajczuk@gmail.com <bartlomiejmikolajczuk@gmail.com>
2024-01-09 19:45:03 +0100

    nullable w isolated

--- BookingService.IntegrationTest/Isolated.cs
+++ BookingService.IntegrationTest/Isolated.cs
@@ -1,7 +1,7 @@
 ﻿using NUnit.Framework;
 using NUnit.Framework.Interfaces;
 using System.Transactions;
-
+#nullable disable
 namespace BookingService.IntegrationTest
 {
     public class Isolated : Attribute, ITestAction
