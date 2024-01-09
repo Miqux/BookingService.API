68a0c2ef8b48e330a99422ae13ebb75c6c3b6063
bartlomiejmikolajczuk@gmail.com <bartlomiejmikolajczuk@gmail.com>
2024-01-09 10:18:59 +0100

    integracja z testowa baza danych

---
+++ BookingService.IntegrationTest/DatabaseProvider.cs
@@ -0,0 +1,22 @@
+﻿using BookingService.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+#nullable disable
+namespace BookingService.IntegrationTest
+{
+    public static class DatabaseProvider
+    {
+        public static BookingServiceContext GetApplicationContext()
+        {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<BookingServiceContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("BookingServiceConnectionString"));
+            return new BookingServiceContext(optionsBuilder.Options);
+        }
+    }
+}
--- BookingService.IntegrationTest/PostRepositoryTest.cs
+++ BookingService.IntegrationTest/PostRepositoryTest.cs
@@ -2,10 +2,7 @@
 using BookingService.Application.Mapper;
 using BookingService.Application.UseCase.Post.Command.CreatedPost;
 using BookingService.Application.UseCase.Post.Queries.GetPosts;
-using BookingService.Infrastructure.Persistence;
 using BookingService.Infrastructure.Persistence.Repository;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
 
 namespace BookingService.IntegrationTest
@@ -14,44 +11,28 @@ namespace BookingService.IntegrationTest
     public class PostRepositoryTest
     {
         [Test, Isolated]
-        public async Task Add_GetValidPost_ShouldReturn3Posts()
+        public async Task Add_GetValidPost_ShouldReturn4Posts()
         {
-            var builder = new ConfigurationBuilder()
-               .SetBasePath(Directory.GetCurrentDirectory())
-               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-
-            IConfigurationRoot configuration = builder.Build();
-
-            var optionsBuilder = new DbContextOptionsBuilder<BookingServiceContext>();
-            optionsBuilder.UseSqlServer(configuration.GetConnectionString("BookingServiceConnectionString"));
-            var context = new BookingServiceContext(optionsBuilder.Options);
             var mapper = new MapperConfiguration(x =>
             {
                 x.AddProfile<MappingConfiguration>();
             }).CreateMapper();
-            var postRepository = new PostRepository(context);
+            var postRepository = new PostRepository(DatabaseProvider.GetApplicationContext());
+
             var result = await new GetPostsHandler(mapper, postRepository).Handle(new GetPostsQuery(), new CancellationToken());
 
-            Assert.That(result.Count, Is.EqualTo(3));
+            Assert.That(result.Count, Is.EqualTo(4));
         }
 
         [Test, Isolated]
         public async Task Add_AddValidPost_ShouldReturnSuccess()
         {
-            var builder = new ConfigurationBuilder()
-               .SetBasePath(Directory.GetCurrentDirectory())
-               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-
-            IConfigurationRoot configuration = builder.Build();
-
-            var optionsBuilder = new DbContextOptionsBuilder<BookingServiceContext>();
-            optionsBuilder.UseSqlServer(configuration.GetConnectionString("BookingServiceConnectionString"));
-            var context = new BookingServiceContext(optionsBuilder.Options);
             var mapper = new MapperConfiguration(x =>
             {
                 x.AddProfile<MappingConfiguration>();
             }).CreateMapper();
-            var postRepository = new PostRepository(context);
+            var postRepository = new PostRepository(DatabaseProvider.GetApplicationContext());
+
             var result = await new CreatedPostCommandHandler(postRepository, mapper)
                 .Handle(new CreatedPostCommand() { Content = "Testowy post", Title = "Testowa zawartość postu" }, new CancellationToken());
 
