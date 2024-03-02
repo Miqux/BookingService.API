050a3cc723a051eeb2bb4cd4b6b5a97ac84bbe02
bartlomiejmikolajczuk@gmail.com <bartlomiejmikolajczuk@gmail.com>
2024-03-02 16:47:30 +0100

    dodanie swagger'a na wersji produkcyjnej

--- BookingService.API/Program.cs
+++ BookingService.API/Program.cs
@@ -70,11 +70,11 @@ builder.Services.ConfigureOptions<JwtBearerOptionsSetup>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseAuthentication();
 
